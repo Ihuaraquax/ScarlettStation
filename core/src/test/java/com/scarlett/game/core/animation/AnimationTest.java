4e35ff674febdb680a059eb9357fed698eebba41
lothar <lotharekon@gmail.com>
2016-05-04 21:31:44 +0200

    Animations refresh after my desktop crashed

---
+++ core/src/test/java/com/scarlett/game/core/animation/AnimationTest.java
@@ -0,0 +1,11 @@
+package com.scarlett.game.core.animation;
+
+import static org.junit.Assert.*;
+
+public class AnimationTest {
+
+    @org.junit.Test
+    public void testGetCurrentRegion() throws Exception {
+
+    }
+}
